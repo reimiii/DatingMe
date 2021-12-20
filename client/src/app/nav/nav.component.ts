d75f70564d73f99523293b586e8657aea4a95a3a
reimiii <imiia75775@gmail.com>
2021-12-20 16:32:16 +0700

    End Of S7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -23,6 +23,7 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
     }, error => {
+      // Invalid Username error
       console.log(error);
       this.toastr.error(error.error)
     })
