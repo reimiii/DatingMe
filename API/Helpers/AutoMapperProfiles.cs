78471956d25db19b504ac3081ca8c18eaa0c3b36
reimiii <imiia75775@gmail.com>
2021-12-28 15:52:04 +0700

    End Of Suffer10

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -22,6 +22,7 @@ namespace API.Helpers
                 .MapFrom(src => src.DateOfBirth
                 .CalculateAge()));
             CreateMap<Photo, PhotoDto>();
+            CreateMap<MemberUpdateDto, AppUser>();
         }
     }
 }
